428686d966a3e9cbdd7d5e08a62b13f85f067d29
belyaev-ay <anton.belyaev@sibit.tech>
2024-07-04 12:55:11 +0700

    Kafka Message Testing

--- kafka-support/src/main/java/pw/avvero/test/kafka/RecordCaptor.java
+++ kafka-support/src/main/java/pw/avvero/test/kafka/RecordCaptor.java
@@ -3,39 +3,53 @@ package pw.avvero.test.kafka;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Awaitility;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 @Slf4j
 public class RecordCaptor implements RecordCaptorAccess {
 
-    private final Map<String, Map<Object, List<RecordSnapshot>>> topicKeyRecords = new ConcurrentHashMap<>();
+    private final Map<String, Map<String, Map<Object, List<RecordSnapshot>>>> topicKeyRecords = new ConcurrentHashMap<>();
+
+    public RecordCaptor() {
+        registerKey("ID");
+    }
+
+    public void registerKey(String name) {
+
+    }
 
     public void capture(RecordSnapshot recordSnapshot) {
         log.debug("[KTS] Record captured for topic {} for key {}\n    Headers: {}\n    Value: {}", recordSnapshot.getTopic(),
                 recordSnapshot.getKey(), recordSnapshot.getHeaders(), recordSnapshot.getValue());
         topicKeyRecords.computeIfAbsent(recordSnapshot.getTopic(), k -> new ConcurrentHashMap<>())
-                .computeIfAbsent(recordSnapshot.getKey(), k -> new CopyOnWriteArrayList<>())
+                .computeIfAbsent("ID", k -> new ConcurrentHashMap<>())
+                .computeIfAbsent(recordSnapshot.getKey() != null ? recordSnapshot.getKey() : "", k -> new CopyOnWriteArrayList<>())
                 .add(recordSnapshot);
     }
 
     @Override
     public List<RecordSnapshot> getRecords(String topic, Object key) {
         return topicKeyRecords.getOrDefault(topic, Collections.emptyMap())
+                .getOrDefault("ID", Collections.emptyMap())
                 .getOrDefault(key, Collections.emptyList());
     }
 
-    public List<RecordSnapshot> getRecords(String topic) {
+    public List<RecordSnapshot> getRecords(String topic, Predicate<RecordSnapshot> predicate) {
         return topicKeyRecords.getOrDefault(topic, Collections.emptyMap())
                 .values().stream()
-                .flatMap(List::stream)
-                .collect(Collectors.toList());
+                .flatMap(m -> m.values().stream())
+                .flatMap(Collection::stream)
+                .filter(predicate)
+                .toList();
     }
 
     public RecordCaptorAccess awaitAtMost(int numberOrRecords, long millis) {
---
+++ kafka-support/src/test/groovy/pw/avvero/test/kafka/RecordCaptorTests.groovy
@@ -0,0 +1,55 @@
+package pw.avvero.test.kafka
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.util.function.Predicate
+
+class RecordCaptorTests extends Specification {
+
+    def "No records are captured"() {
+        setup:
+        def captor = new RecordCaptor()
+        expect:
+        captor.getRecords("topic1", "1").size() == 0
+    }
+
+    @Unroll
+    def "Find captured record by ID"() {
+        setup:
+        def captor = new RecordCaptor()
+        when:
+        captor.capture(RecordSnapshot.builder().topic("topic1").key("1").build())
+        then:
+        captor.getRecords("topic1", key).size() == records
+        where:
+        key || records
+        "1" || 1
+        "0" || 0
+    }
+
+    @Unroll
+    def "Find captured record by header"() {
+        setup:
+        def captor = new RecordCaptor()
+        when:
+        captor.capture(RecordSnapshot.builder().topic("topic1").headers(["header1": "value1"]).build())
+        then:
+        captor.getRecords("topic1", predicateHeader(headerName, headerValue)).size() == records
+        where:
+        headerName | headerValue || records
+        "header1"  | "value1"    || 1
+        "header0"  | "value1"    || 0
+        "header1"  | "value0"    || 0
+
+    }
+
+    def predicateHeader(Object headerName, Object headerValue) {
+        return new Predicate<RecordSnapshot>() {
+            @Override
+            boolean test(RecordSnapshot record) {
+                return record.headers[headerName] == headerValue
+            }
+        }
+    }
+}
